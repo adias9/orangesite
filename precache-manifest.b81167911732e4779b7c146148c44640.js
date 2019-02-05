171ffb3c824c034ad9198987132f879b35a9cfce
adias9 <andreasdias9@gmail.com>
2019-02-05 09:55:24 -0600

    Updates

---
+++ precache-manifest.b81167911732e4779b7c146148c44640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e5d9606ef83e094567f",
+    "url": "/orangesite/static/css/main.ab6d55c9.chunk.css"
+  },
+  {
+    "revision": "9e5d9606ef83e094567f",
+    "url": "/orangesite/static/js/main.9e5d9606.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/orangesite/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d6463ce8ce9c22acb2f0",
+    "url": "/orangesite/static/js/runtime~main.d6463ce8.js"
+  },
+  {
+    "revision": "1e74417750591999d89c6c248f3534e8",
+    "url": "/orangesite/static/media/orange-logo.1e744177.png"
+  },
+  {
+    "revision": "845d221907306ac391c8ca7712447b9e",
+    "url": "/orangesite/index.html"
+  }
+];
