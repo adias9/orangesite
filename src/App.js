fd32f92b9fe24f4d404bdf73fa2f14c8cef4323b
adias9 <andreasdias9@gmail.com>
2019-02-05 09:43:39 -0600

    create orange media landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './orange-logo.png';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Freshly squeezed AR and VR content production. <br/>
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="mailto:orangeinquire@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Inquiries?
           </a>
         </header>
       </div>
